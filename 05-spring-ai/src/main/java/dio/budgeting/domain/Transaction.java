cd7714290a6e3f2cdbafad27eea78bfb43933567
geydsonc <133066283+geydsonc@users.noreply.github.com>
2026-08-23 20:41:33 -0300

    Update Transaction.java

--- 05-spring-ai/src/main/java/dio/budgeting/domain/Transaction.java
+++ 05-spring-ai/src/main/java/dio/budgeting/domain/Transaction.java
@@ -12,6 +12,9 @@ public class Transaction {
     private Category category;
 
     public Transaction(String description, long amount, Category category) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("O valor da transação deve ser positivo e maior que zero.");
+        }
         this.id = new TransactionId();
         this.description = description;
         this.amount = amount;
